058e7d6a9eab4c8c7756624140a16711e4d25720
Pasha127 <pasha.account@gmail.com>
2023-04-17 23:37:34 +0200

    updated pet data

--- marble-manual/src/App.jsx
+++ marble-manual/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PetData from './lib/data/Marble.json'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [petData, setPetData] = useState(PetData)
+
+  
+
 
   return (
     <div className="App">
-      
+     { console.log(petData)}
     </div>
   )
 }
